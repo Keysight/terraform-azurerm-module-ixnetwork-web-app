bd131c7d12c66ab600b15a470f7bce14b619ec84
armdupre <a-joseph.dupre@keysight.com>
2026-02-01 13:29:51 -0500

    Initial Checkin.

---
+++ locals.tf
@@ -0,0 +1,28 @@
+locals {
+	AdminUserName = var.AdminUserName
+	DisablePasswordAuthentication = var.DisablePasswordAuthentication
+	DnsLabel = replace(replace(lower("${local.Preamble}-dns"), "_", "-"), ".", "-")
+	EnableAcceleratedNetworking = true
+	EnableIpForwarding = var.EnableIpForwarding
+	Eth0Name = "${local.Preamble}-eth0"
+	Eth0PublicIpAddressName = "${local.Preamble}-eth0-public-ip"
+	Eth0SubnetId = var.Eth0SubnetId
+	ImagePlanId = local.ImageSku
+	ImageSku = var.ImageSku
+	ImageVersion = var.ImageVersion
+	InstanceId = var.InstanceId
+	InstanceName = "${local.Preamble}-instance"
+	MarketplaceImageOfferId = var.MarketplaceImageOfferId
+	MarketplaceImageProductId = local.MarketplaceImageOfferId
+	MarketplaceImagePublisherId = var.MarketplaceImagePublisherId
+	Preamble = "${local.UserLoginTag}-${local.UserProjectTag}-${local.Tag}-${local.Version}-${local.InstanceId}"
+	ResourceGroupLocation = var.ResourceGroupLocation
+	ResourceGroupName = var.ResourceGroupName
+	SshKeyName = var.SshKeyName
+	Tag = var.Tag
+	UserEmailTag = var.UserEmailTag
+	UserLoginTag = var.UserLoginTag
+	UserProjectTag = var.UserProjectTag
+	Version = var.Version
+	VmSize = var.VmSize
+}
